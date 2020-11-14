873d8b5f3abe6182c21e795a9956c92b2ff80089
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-11-14 14:17:24 -0500

    adde header component

---
+++ gbboshop/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class='title-ctr'>
+    <h1>Great British Bake-off Shop</h1>
+    <p>The Unofficial Online Store for your Favorite Show!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .title-ctr {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    border: dashed 5px pink;
+    margin-top: 2rem;
+    padding: 1rem 2rem;
+  }
+
+</style>
